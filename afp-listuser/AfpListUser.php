bf205962ca27c5ac1c4f09e1dfd17dba62493924
Andre Paiva <61209835+afpaiva@users.noreply.github.com>
2020-06-24 14:50:31 -0300

    Add files via upload

---
+++ afp-listuser/AfpListUser.php
@@ -0,0 +1,125 @@
+<?php
+/*
+Plugin Name: AFP-List User
+Plugin URI: http://www.github.com/afpaiva
+Description: A task proposed by Impsyde. Hope to present a good work. Thanks for this opportunity.
+Version: 1.0
+Author: Andre Fonseca de Paiva
+Author URI: http://www.ensaios3d.com/andrepaiva
+Text Domain: afp-listuser
+Licence: MIT
+*/
+declare (strict_types=1);
+namespace {
+    class AfpListUser
+    {
+    
+        private static $instance;
+    
+        public static function getInstance()
+        {
+            if (self::$instance === null) {
+                self::$instance === new self();
+            }
+        }
+
+        private function __construct()
+        {
+            add_action('admin_menu', array($this, 'customFieldsSet'));
+            add_shortcode('inpsyde-job-test', array($this, 'afpListUsers'));
+        }
+
+        public function afpListUsers() : string
+        {
+            ?><script>
+            var urlInput = "<?= esc_url(get_option('URLtest'))?>";
+          </script><?php
+       
+            $content=  "<link rel=stylesheet type=text/css href=".plugin_dir_url(__FILE__)."css/style.css>";
+            $content.= "<div id=display-users>";
+            $content.= "    <div class=table-open>";
+            $content.= "        <table id=details-table-header></table>";
+            $content.= "        <table id=details-table></table>";
+            $content.= "        <table id=details-table-footer>";
+            $content.= "        </table>";
+            $content.= "    </div>";
+            $content.= "</div>";
+            $content.= "<div id=users-table-container>";
+            $content.= "    <table id=users-table>";
+            $content.= "        <tr style=background-color: #222222; color: white>";
+            $content.= "        <td>ID</td><td>Name</td><td>Username</td>";
+            $content.= "        </tr>";
+            $content.= "    </table>";
+            $content.= "</div>";
+            $content.= "<script>var urlInput = ".'"'.esc_url(get_option('URLtest')).'"'."</script>";
+            $content.= "<script type=text/javascript src=".plugin_dir_url(__FILE__);
+            $content.= "js/afp-scripts.js></script>";
+          
+            return $content;
+        }
+
+        public static function customFieldsSet()
+        {
+            add_menu_page(
+                'Users List',
+                'Users List',
+                'manage_options',
+                'afp-listuser',
+                'AfpListUser::customFieldsSave',
+                'dashicons-buddicons-buddypress-logo',
+                '20'
+            );
+        }
+
+        public static function customFieldsSave()
+        {
+            ?>
+          <h1>List Users</h1>
+          <h3>A test for Inpsyde</h3>
+          Author: Andre Paiva<br><br><br><br>
+          <h3>This is a plugin test.</h3>
+          You can add into a Blog Post, or anywhere on page a dynamic table with users list from the
+          URL bellow:</p>
+          URL provided to the test:<b> http://jsonplaceholder.typicode.com/users </b>
+          <br><br>
+          
+            <?php
+            if (!empty($_POST["URLtest"])) {
+                update_option('URLtest', esc_url($_POST["URLtest"]));
+            } 
+            $urlValue = esc_url(get_option('URLtest'));
+            ?>
+          
+          <form method=POST>
+             URL:<input type=text size=50 name=URLtest value=<?php echo $urlValue?>>
+             <input type=submit value="Test & Save">
+          </form>
+          
+          <div id="message"> <b style=color:green><br> Your URL is working fine! </b></div>
+          
+          <br><br><br>
+          <h3>How to show on page:</h3>
+          <p>Create a post typing <b>[inpsyde-job-test]</b>, the table will be shown on page.</p>
+          
+          <script>
+             localStorage.removeItem('cache-users');
+             var urlInput = "<?php echo esc_url(get_option('URLtest'))?>";
+             var usersRequest = new XMLHttpRequest();
+             usersRequest.open('GET',urlInput);
+             usersRequest.onload = function () {
+                if ((usersRequest.status >=200 && usersRequest.status < 400) || !usersRequest.status){
+                   var usersData = JSON.parse(usersRequest.responseText);
+                }else{
+                   var msg1 = '<br><b style=color:red>There is something wrong in your URL. (';
+                   var msg2 = ')</b><br>Please double check it, so the table will not be shown on page.';
+                   document.getElementById("message").innerHTML = msg1 + usersRequest.status + msg2;
+                }
+             }
+             usersRequest.send();
+             
+          </script>
+            <?php
+        }
+    }
+    AfpListUser::getInstance();
+}
--- afp-listuser/js/afp-scripts.js
+++ afp-listuser/js/afp-scripts.js
@@ -1,117 +1,131 @@
-var usersRequest = new XMLHttpRequest();
-usersRequest.open('GET','http://jsonplaceholder.typicode.com/users');
-usersRequest.onload = function () {
-    var usersData = JSON.parse(usersRequest.responseText);
-    loadUsersList(usersData);
-}
-usersRequest.send();
+var usersData;
 
-function loadUsersList(users){
-    var table = document.getElementById("users-table")
-    for (i = users.length - 1; i >= 0; i--){
-
-        var row = table.insertRow(1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-
-        cell1.innerHTML = "<a style=cursor:pointer onClick=showDetails(" + users[i].id + ");openView()>" + String(users[i].id) + "</a>";
-        cell2.innerHTML = "<a style=cursor:pointer onClick=showDetails(" + users[i].id + ");openView()>" + String(users[i].name) + "</a>";
-        cell3.innerHTML = "<a style=cursor:pointer onClick=showDetails(" + users[i].id + ");openView()>" + String(users[i].username) + "</a>";
-    }
-}
-
-function showDetails(id){
-
+var stored = localStorage.getItem('cache-users');
+if (stored) {
+    usersData = JSON.parse(localStorage.getItem('cache-users'));
+    loadUsersList();
+    console.log('using cache');
+} else {
     var usersRequest = new XMLHttpRequest();
-    usersRequest.open('GET','http://jsonplaceholder.typicode.com/users');
+    usersRequest.open('GET',urlInput);
     usersRequest.onload = function () {
-        var usersData = JSON.parse(usersRequest.responseText);
-        
-        var tableDet = document.getElementById("details-table");
-        var tableDetHead = document.getElementById("details-table-header");
-        var tableDetFooter = document.getElementById("details-table-footer");
-
-        for (i = 0; i < usersData.length; i++){
-            if (usersData[i].id == id){
-
-                var rowDetFooter = tableDetFooter.insertRow(0);
-                var cellDetFooter1 = rowDetFooter.insertCell(0);
-
-                var btnClose = "<button onClick=clearTable();closeView()>close</button> ";
-                var btnNext = "<button onClick=showDetails(" + (id+1) + ");clearTable()>>></button> ";
-                var btnPrev = "<button onClick=showDetails(" + (id-1) + ");clearTable()><<</button> ";
-
-                if (id > usersData.length-1)
-                    btnNext = "<button>--</button> ";
-                if (id < 2)
-                    btnPrev = "<button>--</button> ";
-
-                console.log("id: "+id);
-                console.log("lenght"+usersData.length);
-
-                cellDetFooter1.innerHTML = btnClose + btnPrev + btnNext;
-
-                var rowDetHead = tableDetHead.insertRow(0);
-                var cellDetHead1 = rowDetHead.insertCell(0);
-                var cellDetHead2 = rowDetHead.insertCell(1);
-
-                cellDetHead1.innerHTML = "<h3>#" + usersData[i].id + "</h3>";
-                cellDetHead2.innerHTML = "<h3>" + usersData[i].name + "</h3>";
-
-                var rowDet1 = tableDet.insertRow(0);
-                var cellDet1 = rowDet1.insertCell(0);
-                var rowDet2 = tableDet.insertRow(1);
-                var cellDet2 = rowDet2.insertCell(0);
-                var rowDet3 = tableDet.insertRow(2);
-                var cellDet3 = rowDet3.insertCell(0);
-                var rowDet4 = tableDet.insertRow(3);
-                var cellDet4 = rowDet4.insertCell(0);
-                var rowDet5 = tableDet.insertRow(4);
-                var cellDet5 = rowDet5.insertCell(0);
-                var rowDet6 = tableDet.insertRow(5);
-                var cellDet6 = rowDet6.insertCell(0);
-                var rowDet7 = tableDet.insertRow(6);
-                var cellDet7 = rowDet7.insertCell(0);
-                var rowDet8 = tableDet.insertRow(7);
-                var cellDet8 = rowDet8.insertCell(0);
-                var rowDet9 = tableDet.insertRow(8);
-                var cellDet9 = rowDet9.insertCell(0);
-                var rowDet10 = tableDet.insertRow(9);
-                var cellDet10 = rowDet10.insertCell(0);
-                var rowDet11 = tableDet.insertRow(10);
-                var cellDet11 = rowDet11.insertCell(0);
-                var rowDet12 = tableDet.insertRow(11);
-                var cellDet12 = rowDet12.insertCell(0);
-
-                cellDet1.innerHTML = "<b>Username:</b><i> " + usersData[i].username +"</i>";
-                cellDet2.innerHTML = "<b>E-mail:</b><i> " + usersData[i].email +"</i>";
-                cellDet3.innerHTML = "<b>Phone:</b><i>  " + usersData[i].phone +"</i>";
-                cellDet4.innerHTML = "<b>Website:</b><i>  " + usersData[i].website +"</i>";
-                cellDet5.innerHTML = "<b>Company Name:</b><i>  " + usersData[i].company.name +"</i>";
-                cellDet6.innerHTML = "<b>Catch Phrase:</b><i>  " + usersData[i].company.catchPhrase +"</i>";
-                cellDet7.innerHTML = "<b>BS:</b><i>  " + usersData[i].company.bs +"</i>";
-                cellDet8.innerHTML = "<a href=https://www.google.com/maps/@" + usersData[i].address.geo.lat + "," + usersData[i].address.geo.lng + ",15z target=_blank><b>Street:</b><i>  " + usersData[i].address.city +"</a>";
-                cellDet9.innerHTML = "<b>Suite:</b><i>  " + usersData[i].address.suite +"</i>";
-                cellDet10.innerHTML = "<b>Zip Code:</b><i>  " + usersData[i].address.zipcode +"</i>";
-                cellDet11.innerHTML = "<b>City:</b><i>  " + usersData[i].address.city +"</i>";
-            }
+        try {
+            usersData = JSON.parse(usersRequest.responseText);
+            localStorage.setItem('cache-users', JSON.stringify(usersData));
+            loadUsersList();
+        } catch (err) {
+            console.log('There is an error on URL, unable to find it.');
         }
     }
     usersRequest.send();
 }
-function closeView(){
+
+function loadUsersList()
+{
+
+    var table = document.getElementById("users-table")
+    for (i = usersData.length - 1; i >= 0; i--) {
+        var row = table.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var toId = Number(usersData[i].id)-1;
+
+        cell1.innerHTML = "<a style=cursor:pointer onClick=showDetails(" + toId + ");openView()>" + String(usersData[i].id) + "</a>";
+        cell2.innerHTML = "<a style=cursor:pointer onClick=showDetails(" + toId + ");openView()>" + String(usersData[i].name) + "</a>";
+        cell3.innerHTML = "<a style=cursor:pointer onClick=showDetails(" + toId + ");openView()>" + String(usersData[i].username) + "</a>";
+    }
+}
+
+function showDetails(id)
+{
+  
+    var tableDet = document.getElementById("details-table");
+    var tableDetHead = document.getElementById("details-table-header");
+    var tableDetFooter = document.getElementById("details-table-footer");
+
+    var rowDetFooter = tableDetFooter.insertRow(0);
+    var cellDetFooter1 = rowDetFooter.insertCell(0);
+
+    var btnClose = "<button onClick=clearTable();closeView()>close</button> ";
+    var btnNext = "<button onClick=clearTable();showDetails(" + (id+1) + ")>>></button> ";
+    var btnPrev = "<button onClick=clearTable();showDetails(" + (id-1) + ")><<</button> ";
+
+    if (id >= usersData.length-1) {
+        btnNext = "<button>--</button> ";
+    }
+    if (id <= 1) {
+        btnPrev = "<button>--</button> ";
+    }
+
+    //console.log("id: "+id);
+    //console.log("len: "+usersData.length);
+
+    cellDetFooter1.innerHTML = btnClose + btnPrev + btnNext;
+
+    var rowDetHead = tableDetHead.insertRow(0);
+    var cellDetHead1 = rowDetHead.insertCell(0);
+    var cellDetHead2 = rowDetHead.insertCell(1);
+
+    cellDetHead1.innerHTML = "<h3>#" + usersData[id].id + "</h3>";
+    cellDetHead2.innerHTML = "<h3>" + usersData[id].name + "</h3>";
+
+    var rowDet1 = tableDet.insertRow(0);
+    var cellDet1 = rowDet1.insertCell(0);
+    var rowDet2 = tableDet.insertRow(1);
+    var cellDet2 = rowDet2.insertCell(0);
+    var rowDet3 = tableDet.insertRow(2);
+    var cellDet3 = rowDet3.insertCell(0);
+    var rowDet4 = tableDet.insertRow(3);
+    var cellDet4 = rowDet4.insertCell(0);
+    var rowDet5 = tableDet.insertRow(4);
+    var cellDet5 = rowDet5.insertCell(0);
+    var rowDet6 = tableDet.insertRow(5);
+    var cellDet6 = rowDet6.insertCell(0);
+    var rowDet7 = tableDet.insertRow(6);
+    var cellDet7 = rowDet7.insertCell(0);
+    var rowDet8 = tableDet.insertRow(7);
+    var cellDet8 = rowDet8.insertCell(0);
+    var rowDet9 = tableDet.insertRow(8);
+    var cellDet9 = rowDet9.insertCell(0);
+    var rowDet10 = tableDet.insertRow(9);
+    var cellDet10 = rowDet10.insertCell(0);
+    var rowDet11 = tableDet.insertRow(10);
+    var cellDet11 = rowDet11.insertCell(0);
+    var rowDet12 = tableDet.insertRow(11);
+    var cellDet12 = rowDet12.insertCell(0);
+
+    cellDet1.innerHTML = "<b>Username:</b><i> " + usersData[id].username +"</i>";
+    cellDet2.innerHTML = "<b>E-mail:</b><i> " + usersData[id].email +"</i>";
+    cellDet3.innerHTML = "<b>Phone:</b><i>  " + usersData[id].phone +"</i>";
+    cellDet4.innerHTML = "<b>Website:</b><i>  " + usersData[id].website +"</i>";
+    cellDet5.innerHTML = "<b>Company Name:</b><i>  " + usersData[id].company.name +"</i>";
+    cellDet6.innerHTML = "<b>Catch Phrase:</b><i>  " + usersData[id].company.catchPhrase +"</i>";
+    cellDet7.innerHTML = "<b>BS:</b><i>  " + usersData[id].company.bs +"</i>";
+    cellDet8.innerHTML = "<a href=https://www.google.com/maps/@" + usersData[id].address.geo.lat + "," + usersData[id].address.geo.lng + ",15z target=_blank><b>Street:</b><i>  " + usersData[id].address.city +"</a>";
+    cellDet9.innerHTML = "<b>Suite:</b><i>  " + usersData[id].address.suite +"</i>";
+    cellDet10.innerHTML = "<b>Zip Code:</b><i>  " + usersData[id].address.zipcode +"</i>";
+    cellDet11.innerHTML = "<b>City:</b><i>  " + usersData[id].address.city +"</i>";
+
+}
+function closeView()
+{
     document.getElementById("display-users").style.width = "0%";
     document.getElementById("display-users").style.height = "0%";
     document.getElementById("users-table-container").style.left = "0px";
 }
-function openView(){
+function openView()
+{
     document.getElementById("display-users").style.width = "100%";
     document.getElementById("display-users").style.height = "100%";
     document.getElementById("users-table-container").style.left = "-2000px";
 }
-function clearTable(){
-    for (i = 0; i<12; i++) document.getElementById("details-table").deleteRow(0);
+function clearTable()
+{
+    for (i = 0; i<12; i++) {
+        document.getElementById("details-table").deleteRow(0);
+    }
     document.getElementById("details-table-header").deleteRow(0);
     document.getElementById("details-table-footer").deleteRow(0);
 }
+
